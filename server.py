a851af64ab178fd5ab3fc5a131d968e42d7b185e
Flavia Etukulapati <flavia.etukulapati@orasi.com>
2026-02-13 00:09:51 -0800

    Fix CORS properly

--- server.py
+++ server.py
@@ -55,7 +55,10 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:3000",
+        "https://film-production-backend.onrender.com",
+        ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
